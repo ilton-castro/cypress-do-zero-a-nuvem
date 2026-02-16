2de4af63f7b4c023a59a8b2a6a5fc324529033d7
ilton-castro <ilton.ncastro@gmail.com>
2026-02-16 17:44:08 -0300

    Atualização do codigo

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,4 +1,5 @@
-   it.only('testa a página da política de privacidade de forma independente', () => {
+Cypress._.times(3, () => {
+    it.only('testa a página da política de privacidade de forma independente', () => {
     cy.visit('src/privacy.html')   
         
     cy.contains('h1', 'CAC TAT - Política de Privacidade')
@@ -7,3 +8,5 @@
     cy.contains('p', 'Talking About Testing')
       .should('be.visible')
     })
+
+})
